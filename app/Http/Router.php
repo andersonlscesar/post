b16c86ed04d7e910147596a159e9fa2d6cae5334
Anderson Cesar <andersonlscesar@gmail.com>
2022-12-29 20:19:41 -0300

    criacao de rotas dinamicas

--- app/Http/Router.php
+++ app/Http/Router.php
@@ -3,6 +3,7 @@ namespace App\Http;
 
 use Closure;
 use Exception;
+use ReflectionFunction;
 
 class Router 
 {
@@ -33,9 +34,10 @@ class Router
      * @param string $method
      * @param string $route
      * @param array $params
+     * @return Router
      */
 
-    private function addRouter(string $method, string $route, array $params = [])
+    private function addRouter(string $method, string $route, array $params = []): Router
     {
         foreach($params as $key => $value) {
             if($value instanceof Closure) {
@@ -43,18 +45,29 @@ class Router
                 $params['controller'] = $value;
             }
         }
+
+        $params['variables'] = [];
+        $patterVariables = '/{(.*?)}/';
+
+        if(preg_match_all($patterVariables, $route, $matches)) {
+            $route = preg_replace($patterVariables, '(.*?)', $route);
+            $params['variables'] = $matches[1];
+
+        } 
         
         $patternRoute = '/^'.str_replace('/', '\/', $route).'$/';
-        return $this->routes[$patternRoute][$method] = $params;
+        $this->routes[$patternRoute][$method] = $params;
+        return $this;
     }
 
     /**
      * Método responsável por criar as rotas do sistema
      * @param string $route
      * @param array $params
+     * @return Router
      */
 
-    public function get(string $route, array $params = [])
+    public function get(string $route, array $params = []): Router
     {
         return $this->addRouter('GET', $route, $params);
     }
@@ -83,8 +96,12 @@ class Router
 
         foreach($this->routes as $pattern => $method) {
 
-            if(preg_match($pattern, $uri)) {
+            if(preg_match($pattern, $uri, $matches)) {
                 if(isset($method[$httpMethod])) {
+                    unset($matches[0]);
+                    $keys = $method[$httpMethod]['variables'];
+                    $method[$httpMethod]['variables'] = array_combine($keys, $matches);
+                    $method[$httpMethod]['variables']['request'] = $this->request;
                     return $method[$httpMethod];
                 }
 
@@ -105,7 +122,13 @@ class Router
             }
 
             $args = [];
+            $reflection = new ReflectionFunction($route['controller']);
 
+            foreach($reflection->getParameters() as $parameter) {
+                $name = $parameter->getName();
+                $args[$name] = $route['variables'][$name] ?? '';
+            }
+       
             return call_user_func_array($route['controller'], $args);
 
         } catch(Exception $e) {
@@ -113,5 +136,16 @@ class Router
         }
     }
 
+     /**
+      * Valida se o que está sendo passado no parâmetro da closure é numérico
+      * @param string $param
+      * @return void
+      */
+
+    public function whereNumber(string $param): void 
+    {
+        $route = $this->getRoute()['variables'];
+        if(isset($route[$param]) && !is_numeric($route[$param])) die('Parâmetro inválido'); 
+    }
 
 }
--- routes/home/homeRoutes.php
+++ routes/home/homeRoutes.php
@@ -5,3 +5,5 @@ use App\Controller\Pages;
 $route->get('/home', [
     fn() => new Response(200, Pages\HomeController::renderContent())
 ]);
+
+
